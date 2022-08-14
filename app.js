0680d060c5b989acc3888f59ebe3a50486351ccd
ISTEG0 <87001549+ISTEG0@users.noreply.github.com>
2022-08-14 18:17:27 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ for(let i = 0; i < SQUARES_NUMBER; i++) {
 
     square.addEventListener('mouseover', () => setColor(square))
     square.addEventListener('mouseleave', () => removeColor(square))
+    square.addEventListener('touchstart', () => setColor(square))
+    square.addEventListener('touchend', () => removeColor(square))
 
     board.append(square);
 }
